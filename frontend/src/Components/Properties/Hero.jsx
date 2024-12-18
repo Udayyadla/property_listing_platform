4f3807e831f4f0c67ca8418625518e144b273ceb
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2024-12-18 21:09:00 +0530

    properties component added

---
+++ frontend/src/Components/Properties/Hero.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import '../../Styles/Properties/Hero.css';
+import { Features } from './Features';
+import { ContactUs } from './ContactUs';
+
+
+export const Hero = () => {
+    return (
+        <div className='container'>
+            <div className="hero-section">
+                <div className="hero-content">
+                    <p>Properties</p>
+                    <h3>Looking to Buy, Sell, Rent, Invest or Manage?</h3>
+                </div>
+            </div>
+
+            {/* Features section */}
+            <Features />
+            <ContactUs />
+        </div>
+
+    );
+};
