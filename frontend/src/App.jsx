8fdf13ddf44fd608981e4437086ff2367ccb6cbc
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2024-12-18 21:35:34 +0530

    modified code in hero component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Home } from './components/Home'
+import { Hero } from './Components/Properties/Hero'
 function App() {
   
   return (
     <>
       <Home/>
+      <Hero/>
     </>
   )
 }
