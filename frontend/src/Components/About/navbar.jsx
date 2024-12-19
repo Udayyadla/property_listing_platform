100714b49e3456c7ff3126889137a31a281d31f4
SushmithaM13 <sushmithagoud13@gmail.com>
2024-12-19 13:24:09 +0530

    added navbar and footer

--- frontend/src/Components/About/navbar.jsx
+++ frontend/src/Components/About/navbar.jsx
@@ -3,12 +3,13 @@ import "../../Styles/About/navbar.css"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../assets/Properties_assets/logo.jpg';
 
 export const Navbar=()=>{
     return(
         <div className="navbar">
             <div className="logo">
-                <img src="" alt="logo"/>
+                <img src={logo} alt="logo"/>
             </div>
             <div className="nav-middle">
                 <Link to="/Home">Home</Link>
