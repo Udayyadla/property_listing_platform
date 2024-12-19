dd58d1f046f54f4b7c31878304697c16e58ffa43
Tharun075 <tharunrt570@gmail.com>
2024-12-19 11:59:54 +0530

    changes made to file structure:Home.jsx & Home.css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,4 @@
-import './App.css'
-import { Home } from './components/Home'
+import { Home } from './components/Home/Home'
 import { Hero } from './Components/Properties/Hero'
 function App() {
   
--- frontend/src/Components/Home/Home.jsx
+++ frontend/src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-
+import '../../Styles/Home/Home.css'
 export const Home=()=>{
     return (
         <>
