1392d1276a49f5c7fde2b87777bd46ddc9fbaeb7
SushmithaM13 <sushmithagoud13@gmail.com>
2024-12-19 11:39:01 +0530

    added navbar

---
+++ frontend/src/Components/About/navbar.jsx
@@ -0,0 +1,25 @@
+ import { Link } from 'react-router-dom';
+import "../../Styles/About/navbar.css"
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons';
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+export const Navbar=()=>{
+    return(
+        <div className="navbar">
+            <div className="logo">
+                <img src="" alt="logo"/>
+            </div>
+            <div className="nav-middle">
+                <Link to="/Home">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/Hero">Properties</Link>
+            </div>
+            <div className="nav-icons">
+            <FontAwesomeIcon icon={faTwitter} />
+            <FontAwesomeIcon icon={faYoutube} />
+            <FontAwesomeIcon icon={faSearch} />
+            </div>
+       </div>
+    )
+}
