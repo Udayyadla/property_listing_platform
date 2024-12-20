c93f0da8327c2653486b81e77c12f01f9db8a194
Soumyajit Padhan <soumyajitpadhan15@gmail.com>
2024-12-20 23:13:06 +0530

    modified properties and features

--- frontend/src/Components/Properties/Features.jsx
+++ frontend/src/Components/Properties/Features.jsx
@@ -49,7 +49,7 @@ export const Features = () => {
             </div>
         );
     }
-    
+
     if (error) {
         return (
             <div className="error-container">
@@ -57,39 +57,78 @@ export const Features = () => {
             </div>
         );
     }
-    
+
 
     function handleNavigate(id) {
         navigate(`/propertiesDetails/${id}`)
     }
 
+    async function handleChangeSort(e) {
+        let value = e.target.value;
+        try {
+            const response = await fetch(`https://property-listing-platform.onrender.com/api/getProps?page=${currentPage}&limit=6&sortBy=Price&sortOrder=${value}`);
+            if (!response.ok) {
+                throw new Error(`HTTP Error! Status: ${response.status}`);
+            }
+            const data = await response.json();
+            setProperties(data);
+        }
+        catch (error) {
+            console.log(error);
+        }
+
+    }
+
     return (
-        <div className="features">
-            <h1>Property Listings</h1>
-            <div className="property-grid">
-                {memoizedProperties.map((property) => (
-                    <div className="property-card" key={property._id} onClick={() => handleNavigate(property._id)}>
-                        <img
-                            src={property.Image_URL}
-                            alt={property.Title}
-                            className="property-image"
-                        />
-                        <h2>{property.Title}</h2>
-                        <p>
-                            <strong>Location:</strong> {property.Location}
-                        </p>
-                        <p>
-                            <strong>Status:</strong> {property.Status}
-                        </p>
-                    </div>
-                ))}
-            </div>
-            <div className="pagination">
-                <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-                    Previous
-                </button>
-                <span>Page {currentPage}</span>
-                <button onClick={handleNextPage} disabled={currentPage === 8}>Next</button>
+        <div>
+            <div className="features">
+                <h1>Property Listings</h1>
+                <div>
+                    <select onChange={handleChangeSort}>
+                        <option value="">Sort By Price</option>
+                        <option value="desc">High To Low</option>
+                        <option value="asc">Low To High</option>
+                    </select>
+                </div>
+                <div className="property-grid">
+                    {memoizedProperties.map((property) => (
+                        <div className="property-card-1" key={property._id} onClick={() => handleNavigate(property._id)}>
+                            <img
+                                src={property.Image_URL}
+                                alt={property.Title}
+                                className="property-image"
+                            />
+                            <div className="property-status">
+                                <span className={
+                                    property.Status === "Available"
+                                        ? "for-sale"
+                                        : property.Status === "Pending"
+                                            ? "for-pending"
+                                            : "for-sold"
+                                }>
+                                    {property.Status}
+                                </span>
+                            </div>
+                            <div className="property-details">
+                                <h2>{property.Title}</h2>
+                                <p>{property.Location}</p>
+                                <p><strong>Price:</strong> ₹{property.Price}</p>
+                                <div className="property-specs">
+                                    <span>{property.Size}  Sqft</span>
+                                    <span>{property.Bedrooms}  Bed</span>
+                                    <span>{property.Bathrooms}  Bath</span>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+                <div className="pagination">
+                    <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+                        Previous
+                    </button>
+                    <span>Page {currentPage}</span>
+                    <button onClick={handleNextPage} disabled={currentPage === 7}>Next</button>
+                </div>
             </div>
         </div>
     );
