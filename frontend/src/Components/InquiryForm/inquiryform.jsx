d2cc38315bd3357ae240df421c464a801f7e534e
Yadla Uday Kumar <udaykumar3439@gmail.com>
2024-12-21 22:51:28 +0530

    inquiryForm.css file renamed

--- frontend/src/Components/InquiryForm/inquiryform.jsx
+++ frontend/src/Components/InquiryForm/inquiryform.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "../../Styles/About/InquiryForm.css";
+import "../../Styles/About/inquiryForm.css";
 
 export const InquiryForm = () => {
   const [formData, setFormData] = useState({
